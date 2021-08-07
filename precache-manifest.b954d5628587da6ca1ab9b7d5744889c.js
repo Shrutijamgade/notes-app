93c35afc2427d23804bb5faae1f81b6194e71b38
Shruti <shrutijp@microland.com>
2021-08-07 17:42:15 +0530

    Updates

---
+++ precache-manifest.b954d5628587da6ca1ab9b7d5744889c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e0c352d4201e3f9764a18135e46f52",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "269215dcd0102a9113c4",
+    "url": "/notes-app/static/js/2.3c968d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notes-app/static/js/2.3c968d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aae2ae4711f4c016d0c",
+    "url": "/notes-app/static/js/main.7b01ee79.chunk.js"
+  },
+  {
+    "revision": "9c52fc73b803e2a8fdf3",
+    "url": "/notes-app/static/js/runtime-main.c0afa376.js"
+  }
+]);
